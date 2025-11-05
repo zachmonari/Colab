6a07efdabd10c1fa73f45353c4a1f1165aff6230
zachmonari <zachmonari002@gmail.com>
2025-11-05 06:46:03 +0000

    Editted a script from Google Colab

--- colab_script.py
+++ colab_script.py
@@ -4,3 +4,6 @@ print("Hello from Colab!")
 def my_function():
     return "This code was pushed from Google Colab"
 my_function()
+def my_function1():
+    return "The exercise was successful!"
+my_function1()
